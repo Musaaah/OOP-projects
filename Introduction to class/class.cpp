5165303c31420fc47df76fbf96b0ed03be7593aa
Musaaah <musaaah74@gmail.com>
2025-02-11 10:53:08 +0300

    Add files via upload

---
+++ Introduction to class/class.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class BankAccount
+{
+public:// public access specifier
+string name;
+int balance;
+
+void withdraw(int amount)
+{
+    balance= balance - amount;
+}
+
+};
+
+
+
+int main()
+{
+  // BankAccount account1;
+  // account1.name="Musaaah";
+  // account1.balance=5000;
+//cout<< account1.name << "has"  <<  account1.balance <<  "shillings"<<endl;
+
+BankAccount account2;
+account2.name="Brayo";
+account2.balance=6788;
+
+cout << account2.name << "has" << account2.balance << "shillings"<<endl;
+account2.withdraw(100);
+cout << account2.name << "has" << account2.balance << "shillings"<<endl;
+
+
+    return 0;
+}
+
---
+++ volume/Volume of a cylinder.cpp
@@ -0,0 +1,22 @@
+//name
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+const double PI = 3.14159265358979323846;
+
+double calculateVolume(double radius, double height) {
+    return PI * pow(radius, 2) * height;
+}
+int main() {
+    double radius, height;
+    cout << "Enter the radius of the cylinder: ";
+    cin >> radius;
+    cout << "Enter the height of the cylinder: ";
+    cin >> height;
+    double volume = calculateVolume(radius, height);
+    cout << "The volume of the cylinder is: " << volume << " cubic units" << endl;
+    return 0;
+}
+
